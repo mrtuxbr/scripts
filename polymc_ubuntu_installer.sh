f9424ff036c4420795d3b5f3e30509fca47dba60
Mr.TuX.br <87543218+mrtuxbr@users.noreply.github.com>
2022-07-06 13:58:54 -0300

    Create polymc_ubuntu_installer.sh

---
+++ polymc_ubuntu_installer.sh
@@ -0,0 +1,11 @@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@ PolyMC Installer for Ubunutu@@
+echo @@    Developed by Mr.TuX.Br   @@
+echo @@           Version: 1        @@
+echo @@  Published on July 6th 2022 @@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+sleep 5
+curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update
+sudo apt install polymc -y
